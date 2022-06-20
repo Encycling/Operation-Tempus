32b97762ad584470f28a828040a5612393c39757
Wilson Thai <wilsonhthai@gmail.com>
2022-06-19 23:21:44 -0700

    testing some stuff

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// W.I.P
-		System.out.println("hi");
+		System.out.println("This project will start once Chez is finished learning Java");
 	}
 
 }
