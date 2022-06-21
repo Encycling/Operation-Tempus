b0c775edf0dfef176dc7094ae136a9a261a92b35
Wilson Thai <wilsonhthai@gmail.com>
2022-06-20 17:27:55 -0700

    eclipse github kinda sucky

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// W.I.P
 		System.out.println("This project will start once Chez is finished learning Java");
+		System.out.println("Eclipse test");
 	}
 
 }
