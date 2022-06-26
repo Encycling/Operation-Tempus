6be862a025e5db937ef6b5b4085c7f114801ede0
Wilson Thai <wilsonhthai@gmail.com>
2022-06-25 22:30:12 -0700

    removed useless things, still W.I.P

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// W.I.P
 		System.out.println("This project will start once Chez is finished learning Java");
-		System.out.println("eclipse is gay with github");
+		System.out.println("Decided to delete the testing class for some reason lmao");
 	}
 
 }
--- src/Testing.java
+++
@@ -1,6 +0,0 @@
-
-public class Testing {
-    void lol() {
-        System.out.println("this aint done bruh");
-    }
-}
