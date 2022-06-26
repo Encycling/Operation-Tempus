87a3870ebce16ef7d4d88487d5e8b6b8242c88ec
Wilson Thai <wilsonhthai@gmail.com>
2022-06-25 23:10:22 -0700

    gonna stop with the joke commits.

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// W.I.P
 		// This project will start once Chez_Poofz is done learning Java.
-		// Will upload updates once this project starts getting serious
+		// Will upload updates once this project starts getting serious.
 	}
 
 }
