cf073b1e173bc350cc197377c2f1dfe9704e6982
Encycling <wilsonhthai@gmail.com>
2022-06-30 23:33:50 -0700

    test

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 		// W.I.P
 		// This project will start once Chez_Poofz is done learning Java.
-		// NOT FINISHEDDDDDDDDD
-		System.out.println("testing");
+		// NOT FINISHED
+		System.out.println("Not finished");
 	}
 
 }
