6090220eb8b324599789188800cf4c3260ccb7f9
Encycling <wilsonhthai@gmail.com>
2022-07-03 21:26:12 -0400

    the first frame. FINALLY....

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
 
 public class Main {
 
@@ -7,63 +7,16 @@ public class Main {
 		// W.I.P
 		// This project will start once Chez_Poofz is done learning Java.
 		// NOT FINISHED
-		
-		// I'm gonna make a random calculator program to pass the time
-		// until operation tempus actually starts lmao
-		
-		Scanner scanner = new Scanner(System.in);
-		
-		char op;
-		int num1;
-		int num2;
-		// basic java shit.
-		try {
-			System.out.print("Enter a number: ");
-			num1 = scanner.nextInt();
-			
-			System.out.print("Enter another number: ");
-			num2 = scanner.nextInt();
-			
-			System.out.println("Example: +,-,/,*");
-			System.out.print("Enter an operator: ");
-			op = scanner.next().charAt(0);
-			
-			switch(op) {
-			case '+':
-				System.out.println(num1 + num2);
-				break;
-			case '-':
-				System.out.println(num1 - num2);
-				break;
-			case '*':
-				System.out.println(num1 * num2);
-				break;
-			case '/':
-				System.out.println(num1 / num2);
-				break;
-			default:
-				System.out.println("Invalid operator.");
-				break;
-			}
-			
-		}
-		// if you try to divide by zero, this message will show up.
-		catch(ArithmeticException e) {
-			System.out.println("Can't divide by zero.");
-		}
-		// if you enter a string instead of a number, this will show up.
-		catch(InputMismatchException e) {
-			System.out.println("Enter a proper number.");
-		}
-		// If theres another problem, this will show up.
-		catch(Exception e) {
-			System.out.println("You messed something up.");
-		}
-		// closing the scanner.
-		finally{
-			scanner.close();
-		}
-		
+
+		// The first frame.
+
+		JFrame frame = new JFrame();
+		frame.setVisible(true);
+		frame.setSize(500, 500);
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		frame.setTitle("Operation T.E.M.P.U.S");
+
 	}
 
 }
